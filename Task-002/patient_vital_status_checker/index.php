0d82682e4a27d5eba99674cb449c6dad7d0be964
Dharani-ravikumar <80830841+Dharani-ravikumar@users.noreply.github.com>
2026-04-29 12:25:37 +0530

    Add files via upload

--- Task-002/patient_vital_status_checker/index.php
+++ Task-002/patient_vital_status_checker/index.php
@@ -1,1 +1,34 @@
+<?php
 
+include "vitals.php";
+include "validate.php";
+include "rules.php";
+include "scanner.php";
+
+foreach ($vitalData as $vital) {
+
+    switch ($vital["vital_type"]) {
+        case "Temperature":
+            $result = validateVital($vital, "checkTemperature");
+            break;
+
+        case "Pulse":
+            $result = validateVital($vital, "checkPulse");
+            break;
+
+        case "BP":
+            $result = validateVital($vital, "checkBloodPressure");
+            break;
+    }
+
+    echo "Patient: " . $result["patient_name"] . "<br>";
+    echo "Vital: " . $result["vital_type"] . "<br>";
+    echo "Value: " . $result["value"] . "<br>";
+    echo "Status: " . $result["status"] . "<br>";
+    echo "Message: " . $result["message"] . "<br>";
+    echo "----------------------<br>";
+}
+
+echo "<br>Project Files:<br>";
+
+scanFolder(__DIR__);
---
+++ Task-002/patient_vital_status_checker/vitals.php
@@ -0,0 +1,25 @@
+<?php
+
+$vitalData = [
+
+    [
+    
+        "patient_name" => "Arun",
+        "vital_type"   => "Temperature",
+        "value"        => "102"
+    ],
+    [
+        "patient_name" => "Fenix",
+        "vital_type"   => "Pulse",
+        "value"        => "110"
+    ],
+    [
+        "patient_name" => "Kumar",
+        "vital_type"   => "BP",
+        "value"        => "120/80"
+    ]
+];
+
+
+
+?>
