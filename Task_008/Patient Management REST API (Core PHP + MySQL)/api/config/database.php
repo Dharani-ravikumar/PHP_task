a6c9c2f9c7cb88d33ebd46c900c36c737df21fbb
Dharani-ravikumar <80830841+Dharani-ravikumar@users.noreply.github.com>
2026-05-21 09:36:37 +0530

    Add files via upload

---
+++ Task_008/Patient Management REST API (Core PHP + MySQL)/api/config/database.php
@@ -0,0 +1,11 @@
+<?php 
+$host="localhost";
+$username="root";
+$password="";
+$database="a_hospital_db";
+$conn= mysqli_connect($host,$username,$password,$database);
+if(!$conn)
+    {
+        die("connection failed");
+    }
+?>
